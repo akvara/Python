8e4241f357986594c29406d9b721de94522e8ceb
Bogdan Popescu <68062990+bopopescu@users.noreply.github.com>
2020-07-22 07:08:51 +0300

    Discard master/slave language

--- lists.py
+++ lists.py
@@ -2,14 +2,14 @@ rest = {'hostname': '172.31.52.141', 'platform': 'Amazon Linux (64 bit)', 'lastM
 soap = {"ID": 350, "description": None, "name": "172.31.52.141", "displayName": "LRV-PRD-EC2-RNGINX-0002", "external": True, "externalID": None, "hostGroupID": 1, "hostType": "STANDARD", "platform": "Amazon Linux (64 bit) (4.9.27-14.31.amzn1.x86_64)", "securityProfileID": 35, "antiMalwareClassicPatternVersion": "N/A", "antiMalwareEngineVersion": "N/A", "antiMalwareIntelliTrapExceptionVersion": "N/A", "antiMalwareIntelliTrapVersion": "N/A", "antiMalwareSmartScanPatternVersion": "N/A", "antiMalwareSpywarePatternVersion": "N/A", "cloudObjectImageId": "ami-95eaf1f3", "cloudObjectInstanceId": "i-0f8b27c5f614d3ad3", "cloudObjectInternalUniqueId": "i-0f8b27c5f614d3ad3", "cloudObjectSecurityGroupIds": "LRV_PRD_SG_ELK-SW-01, LRV-PRD-SG-RNGINX-01", "cloudObjectType": "AMAZON_VM", "componentKlasses": {"item": []}, "componentNames": {"item": []}, "componentTypes": {"item": []}, "componentVersions": {"item": []}, "hostGroupName": "Computers > LayerV PROD Account - EU (Ireland)", "hostInterfaces": {"item": [{"ID": None, "description": None, "name": "eth0", "displayName": None, "external": False, "externalID": None, "hostGroupID": None, "hostType": None, "platform": None, "securityProfileID": None, "dhcp": False, "hostBridgeId": None, "interfaceTypeId": None, "mac": "02:A7:68:8F:E8:12", "notAvailable": False, "virtualDeviceKey": None}]}, "hostLight": "GREEN", "lastAnitMalwareScheduledScan": None, "lastAntiMalwareEvent": None, "lastAntiMalwareManualScan": None, "lastDpiEvent": "2017-10-09 11:00:24.467000+00:00", "lastFirewallEvent": "2017-10-02 14:21:53.437000+00:00", "lastIPUsed": "172.31.68.6", "lastIntegrityMonitoringEvent": None, "lastLogInspectionEvent": None, "lastWebReputationEvent": None, "light": 0, "locked": False, "overallAntiMalwareStatus": "Anti-Malware: Off, not installed, no configuration", "overallDpiStatus": "Intrusion Prevention: On, Detect, 87 rules", "overallFirewallStatus": "Firewall: Off, installed, no rules", "overallIntegrityMonitoringStatus": "Integrity Monitoring: On, 9 rules", "overallLastRecommendationScan": None, "overallLastSuccessfulCommunication": "2017-10-09 11:14:32.203000+00:00", "overallLastSuccessfulUpdate": "2017-10-04 10:12:48.700000+00:00", "overallLastUpdateRequired": "2017-10-04 10:12:15.833000+00:00", "overallLogInspectionStatus": "Log Inspection: Off, not installed, no rules", "overallStatus": "Managed (Online)", "overallVersion": "9.6.2.8140", "overallWebReputationStatus": "Web Reputation: Off, not installed", "securityProfileName": "LRV-PRD-NGINX", "virtualName": None, "virtualUuid": None}
 
 
-def join_lists(master_list, slave_list, keys_list):
-    slave_dict = dict((item['id'], item) for item in slave_list)
+def join_lists(main_list, subordinate_list, keys_list):
+    subordinate_dict = dict((item['id'], item) for item in subordinate_list)
     arr = list()
-    for item in master_list:
+    for item in main_list:
         augmentable = item
         for k in keys_list:
-            if item['id'] in slave_dict:
-                augmentable[k] = slave_dict[item['id']][k]
+            if item['id'] in subordinate_dict:
+                augmentable[k] = subordinate_dict[item['id']][k]
         arr.append(augmentable)
     return arr
 
